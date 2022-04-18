e95e92c134f0a3a75adfb6b9686ea6df24a17967
AndreyKorolevich <korolevich1994@mail.ru>
2022-04-18 21:31:39 +0300

    add modules, rotes and first components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { AppBar, Container, Typography } from '@material-ui/core'
+import flashback from './img/flashback.png'
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>Flashback</Typography>
+        <img src={flashback} alt='flashback' height='60'/>
+      </AppBar>
+
+    </Container>
   )
 }
 
