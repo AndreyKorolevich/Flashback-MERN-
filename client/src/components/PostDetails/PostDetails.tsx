acb9829a06ae64c3a1d6cd7d047d22ba8a67c120
AndreyKorolevich <korolevich1994@mail.ru>
2022-05-06 21:10:06 +0300

    add styles for details page

--- client/src/components/PostDetails/PostDetails.tsx
+++ client/src/components/PostDetails/PostDetails.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
 import styles from './ScssPostDetails.module.scss'
+import useStyles from './materialStyles';
+import moment from 'moment'
 import { useParams, Link } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks'
 import { getCertainPostSelector, getFetchingPostsSelector, getPostsSelector } from '../../selectors/postsSelectors'
 import { CircularProgress, Divider, Paper, Typography } from '@material-ui/core'
-import moment from 'moment'
 import { getCertainPostThunk } from '../../actions/postsAction'
 
 
@@ -12,6 +13,7 @@ type PostDetailsType = {}
 
 const PostDetails: React.FC<PostDetailsType> = () => {
   const dispath = useAppDispatch()
+  const classes = useStyles();
   const posts = useAppSelector(getPostsSelector)
   const post = useAppSelector(getCertainPostSelector)
   const isLoading = useAppSelector(getFetchingPostsSelector)
@@ -24,29 +26,26 @@ const PostDetails: React.FC<PostDetailsType> = () => {
   return (
     isLoading ? <CircularProgress className={styles.progress}/> : (
       <Paper style={{ padding: '20px', borderRadius: '15px' }} elevation={6}>
-        <div className={styles.card}>
+        <div className={classes.card}>
           <div className={styles.section}>
             <Typography variant="h3" component="h2">{post?.title}</Typography>
             <Typography gutterBottom variant="h6" color="textSecondary" component="h2">{post?.tags.map((tag) => (
-              <Link to={`/tags/${tag}`} style={{ textDecoration: 'none', color: '#3f51b5' }}>
+              <Link to={`/tags/${tag}`} key={tag} className={styles.tag}>
                 {` #${tag} `}
               </Link>
             ))}
             </Typography>
-            <Typography gutterBottom variant="body1" component="p">{post?.message}</Typography>
-            <Typography variant="h6">
+            <Typography className={styles.message} gutterBottom variant="body1" component="p">{post?.message}</Typography>
+            <Typography className={styles.creatBy} variant="h6">
               Created by:
-              <Link to={`/creators/${post?.name}`} style={{ textDecoration: 'none', color: '#3f51b5' }}>
+              <Link to={`/creators/${post?.name}`}  className={styles.name}>
                 {` ${post?.name}`}
               </Link>
             </Typography>
-            <Typography variant="body1">{moment(post?.createAt).fromNow()}</Typography>
-            <Divider style={{ margin: '20px 0' }}/>
-            <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
-            <Divider style={{ margin: '20px 0' }}/>
+            <Typography className={styles.created} variant="body1">{moment(post?.createAt).fromNow()}</Typography>
             <Divider style={{ margin: '20px 0' }}/>
           </div>
-          <div className={styles.imageSection}>
+          <div className={classes.imageSection}>
             <img className={styles.media}
                  src={post?.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'}
                  alt={post?.title}/>
---
+++ client/src/components/PostDetails/materialStyles.ts
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  card: {
+    display: 'flex',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+    },
+  },
+  imageSection: {
+    marginLeft: '20px',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 0,
+    },
+  },
+  recommendedPosts: {
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+}));
